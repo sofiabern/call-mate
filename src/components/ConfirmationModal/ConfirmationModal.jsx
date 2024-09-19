4bc4a8324f2494bed5909258b0a3aec7f47225a9
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-19 21:55:02 +0300

    contacts

---
+++ src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -0,0 +1,54 @@
+import Modal from "react-modal";
+import clsx from "clsx";
+
+import css from "./ConfirmationModal.module.css";
+
+const customStyles = {
+  overlay: {
+    backgroundColor: "#00000066",
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    border: "1px solid #ba88f8",
+    borderRadius: "8px",
+    backgroundColor: "#e9d8ff",
+  },
+};
+
+Modal.setAppElement("#root");
+
+const ConfirmationModal = ({ isOpen, onRequestClose, onConfirm }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={customStyles}
+      contentLabel="Confirm Deletion"
+    >
+      <h2 className={css.text}>
+        Are you sure you want to delete this contact?
+      </h2>
+      <div className={css["btn-wrapper"]}>
+        <button
+          onClick={onRequestClose}
+          className={clsx(css.btn, css["btn-cancel"])}
+        >
+          Cancel
+        </button>
+        <button
+          onClick={onConfirm}
+          className={clsx(css.btn, css["btn-delete"])}
+        >
+          Delete
+        </button>
+      </div>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { changeFilter } from "../../redux/filters/slice";
+import { selectFilter } from "../../redux/filters/selectors";
+import css from "./SearchBox.module.css";
+
+function SearchBox() {
+  const dispatch = useDispatch();
+  const nameFilter = useSelector(selectFilter);
+
+
+
+  return (
+      <input
+        className={css.input}
+        placeholder="Search by name or phone number"
+        type="text"
+        value={nameFilter}
+        onChange={e => dispatch(changeFilter(e.target.value))}
+      />
+  );
+}
+
+export default SearchBox;
