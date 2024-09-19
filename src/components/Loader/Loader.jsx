484d86ff8cf91840a5dbf995d72c0dfb67e7ff1a
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-19 21:56:59 +0300

    app and others

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,21 @@
+import { Oval } from "react-loader-spinner";
+
+import css from "./Loader.module.css";
+
+function Loader() {
+  return (
+    <Oval
+      visible={true}
+      height="60"
+      width="60"
+      strokeWidth="4"
+      color="#ba88f8"
+      secondaryColor="#ffffff"
+      ariaLabel="oval-loading"
+      wrapperStyle={{}}
+      wrapperClass={css.loader}
+    />
+  );
+}
+
+export default Loader;
